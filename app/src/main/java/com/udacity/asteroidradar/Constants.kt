97578c5a6dda01b66478ad96877070b3c3dbaae5
mohammed al sharif <ffmm972006@gmail.com>
2023-07-18 12:37:53 +0300

    Update Constants.kt

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,7 +5,6 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
 
-    // please replace myApiNasa in build.gradle(:app) file with your API key
     const val API_KEY = BuildConfig.NASA_API_KEY
 
 }
